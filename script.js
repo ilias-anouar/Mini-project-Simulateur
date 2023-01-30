862f678c44465e73e381bbd52d142b0418d3f2d1
ilias anouar <ilas.anouar.solicode@gmail.com>
2023-01-30 10:42:02 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+let input = document.querySelectorAll("input")
+function setvalue(array) {
+    for (let i = 0; i < array.length; i++) {  
+        array[i].addEventListener("input", (e) => {
+            
+        });
+    }
+}
