dc8982c24d47c9c180d38ee9dc33f3ec3b6a640e
ilias anouar <ilas.anouar.solicode@gmail.com>
2023-01-30 12:19:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-let input = document.querySelectorAll('input')
+let input = document.querySelectorAll("input");
+let inputRGX = /[0-9]/;
 function setvalue(array) {
-    for (let i = 0; i < array.length - 1; i++) {
-        array[i].addEventListener('input', function () {
-            if (i % 2 != 0) {
-                array[i - 1].value = 0
-            } else {
-                array[i + 1].value = 0
-            }
-        })
-    }
+  for (let i = 0; i < array.length - 1; i++) {
+    array[i].addEventListener("input", function (e) {
+      if (i % 2 != 0) {
+        if (inputRGX.test(array[i - 1].value)) {
+          return true;
+        } else {
+          array[i - 1].value = 0;
+        }
+      } else {
+        if (inputRGX.test(array[i + 1].value)) {
+          return true;
+        } else {
+          array[i + 1].value = 0;
+        }
+      }
+    });
+  }
 }
-setvalue(input)
+setvalue(input);
