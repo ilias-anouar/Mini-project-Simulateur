390c468c24b6f5bd7435361a8b163383455ff495
ilias anouar <ilas.anouar.solicode@gmail.com>
2023-01-30 12:14:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 let input = document.querySelectorAll("input");
-let inputRGX = /\d+/g;
+let inputRGX = /[0-9]/;
 function setvalue(array) {
   for (let i = 0; i < array.length - 1; i++) {
     array[i].addEventListener("input", function (e) {
       if (i % 2 != 0) {
         if (inputRGX.test(array[i - 1].value)) {
-          return false;
+          return true;
         } else {
           array[i - 1].value = 0;
         }
       } else {
         if (inputRGX.test(array[i + 1].value)) {
-          return false;
+          return true;
         } else {
           array[i + 1].value = 0;
         }
